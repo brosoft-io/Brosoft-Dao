d44eb163ee241ff532edd1eeb872a14f1210237b
Dillon Dickerson <diltdicker@gmail.com>
2019-07-25 23:41:48 -0700

    fix uri for gitlab postgres

--- src/test/java/cucumber/constants/CucumberConstants.java
+++ src/test/java/cucumber/constants/CucumberConstants.java
@@ -6,7 +6,7 @@ public abstract class CucumberConstants {
 	public static final String ENV_VALUE = "gitlab";
 	public static final String SQLITE_URI = "jdbc:sqlite:test.db";
 	public static final String POSTGRES_URI = "jdbc:postgresql://localhost:5432/test";
-	public static final String POSTGRES_GITLAB_URI = "jdbc:postgresql://potgres:5432/test";
+	public static final String POSTGRES_GITLAB_URI = "jdbc:postgresql://postgres:5432/test";
 	public static final String MONGO_URI = "localhost";
 	public static final String MONGO_GITLAB_URI = "mongo";
 	public static final int MONGO_PORT = 27017;
